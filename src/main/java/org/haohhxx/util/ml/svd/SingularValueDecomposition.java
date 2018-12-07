8020f39d10b78a38ffad1821a5466c8425737f41
hhxx2015 <zhenyuan_hao@163.com>
2018-12-07 18:53:23 +0800

    ver

--- src/main/java/org/haohhxx/util/ml/svd/SingularValueDecomposition.java
+++ src/main/java/org/haohhxx/util/ml/svd/SingularValueDecomposition.java
@@ -5,11 +5,14 @@ package org.haohhxx.util.ml.svd;
  * @author zhenyuan_hao@163.com
  * 奇异值分解
  *
+ * 如何理解矩阵特征值？ https://www.zhihu.com/question/21874816/answer/181864044
  * http://blog.chinaunix.net/uid-20761674-id-4040274.html
  */
 public class SingularValueDecomposition {
 
     public static void main(String[] args) {
+
+
 
     }
 
