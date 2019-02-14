96d1b1a89ac639ad805cc153c27f585cf8110c7e
hhxx2015 <zhenyuan_hao@163.com>
2019-02-14 22:17:26 +0800

    k

--- src/main/java/org/haohhxx/util/ml/hmm/Viterbi.java
+++ src/main/java/org/haohhxx/util/ml/hmm/Viterbi.java
@@ -2,6 +2,19 @@ package org.haohhxx.util.ml.hmm;
 
 public class Viterbi {
 
+    int[] compute(int[] observations,
+                  int[] states,
+                  double[] startProbability,
+                  double[][] transitionProbability,
+                  double[][] emissionProbability
+    ){
+
+
+
+
+    }
+
+
     public static void main(String[] args) {
 
     }
